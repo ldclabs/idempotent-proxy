c4ab0e5f0518420f3e02484784fd22f9a4c323ce
0xZensh <txr1883@gmail.com>
2024-04-08 15:44:09 +0800

    feat: add access_token verifying

--- src/handler.rs
+++ src/handler.rs
@@ -5,9 +5,12 @@ use axum::{
     extract::{Request, State},
     response::{IntoResponse, Response},
 };
+use base64::{engine::general_purpose, Engine};
+use bitcoin::{key::Secp256k1, secp256k1};
 use hyper::{HeaderMap, StatusCode};
 use reqwest::Client;
 use serde_json::{from_reader, Map, Value};
+use sha3::{Digest, Sha3_256};
 use std::sync::Arc;
 
 #[derive(Clone)]
@@ -52,6 +55,38 @@ pub async fn proxy(
             StatusCode::BAD_REQUEST,
             "missing idempotency-key header".to_string(),
         ));
+    }
+
+    // auth
+    if let Ok(pub_key) = std::env::var("TOKEN_PUB_KEY") {
+        let pub_key = general_purpose::URL_SAFE_NO_PAD
+            .decode(pub_key.as_bytes())
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+        let token = extract_header(req.headers(), "authorization", || "".to_string());
+        if !token.starts_with("Bearer ") {
+            return Err((StatusCode::UNAUTHORIZED, "missing Bearer token".to_string()));
+        }
+        let token = general_purpose::URL_SAFE_NO_PAD
+            .decode(token.strip_prefix("Bearer ").unwrap().as_bytes())
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+        let token: (Vec<u8>, Vec<u8>) = ciborium::from_reader(token.as_slice())
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+        let digest = sha3_256(&token.0);
+        let secp = Secp256k1::verification_only();
+        let signature = secp256k1::schnorr::Signature::from_slice(&token.1)
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+        let pub_key = secp256k1::PublicKey::from_slice(&pub_key)
+            .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+        if secp
+            .verify_schnorr(
+                &signature,
+                &secp256k1::Message::from_digest_slice(&digest).unwrap(),
+                &pub_key.into(),
+            )
+            .is_err()
+        {
+            return Err((StatusCode::UNAUTHORIZED, "invalid access_token".to_string()));
+        }
     }
 
     let idempotency_key = format!("{}:{}", method, idempotency_key);
@@ -204,4 +239,10 @@ pub fn extract_header(hm: &HeaderMap, key: &str, or: impl FnOnce() -> String) ->
             Err(_) => or(),
         },
     }
+}
+
+pub fn sha3_256(data: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha3_256::new();
+    hasher.update(data);
+    hasher.finalize().into()
 }
